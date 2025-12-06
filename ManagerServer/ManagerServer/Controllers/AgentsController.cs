c787798b24cfc0071eb8866c96957cf4f82fb61f
HatsuneFikuMiku <d.mazurowskimajcher@gmail.com>
2025-12-07 00:55:07 +0100

    Dodałem model i kontroler Agent do obsługi w bazie danych

---
+++ ManagerServer/ManagerServer/Controllers/AgentsController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using ManagerServer.Models;
+
+namespace ManagerServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AgentsController : ControllerBase
+    {
+        private readonly IMongoCollection<Agent> _agents;
+
+        public AgentsController()
+        {
+            var client = new MongoClient("mongodb://localhost:27017"); 
+            var database = client.GetDatabase("timeseriesdb");
+            _agents = database.GetCollection<Agent>("agents");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAgentsToList()
+        {
+            var agentsList = await _agents.Find(Builders<Agent>.Filter.Empty).ToListAsync();
+            return Ok(agentsList);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> AddAgent([FromBody] Agent agent)
+        {
+            var exists = await _agents.Find(a => a.AgentId == agent.AgentId).FirstOrDefaultAsync();
+            if (exists != null)
+                return Conflict(new { message = "Agent with this Id already exists." });
+
+            await _agents.InsertOneAsync(agent);
+            return Ok(agent);
+        }
+
+        [HttpPut("{agentId}")]
+        public async Task<IActionResult> UpdateToken(string agentId, [FromBody]string newToken)
+        {
+            var builder = Builders<Agent>.Update.Set(u => u.AuthToken, newToken);
+            var update = await _agents.UpdateOneAsync(a => a.AgentId == agentId, builder);
+
+            if(update.MatchedCount == 0)
+                return NotFound(new { message = $"Agent {agentId} not found." });
+
+            return Ok(new { message = $"Token updated for agent {agentId}." });
+        }
+
+        [HttpDelete("{agentId}")]
+        public async Task<IActionResult> DeleteAgent(string agentId)
+        {
+            var delete = await _agents.DeleteOneAsync(a => a.AgentId == agentId);
+
+            if (delete.DeletedCount == 0)
+                return NotFound(new { message = $"Agent {agentId} not found." });
+
+            return Ok(new { message = $"Agent {agentId} deleted." });
+        }
+
+    }
+}
---
+++ ManagerServer/ManagerServer/Models/Agent.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ManagerServer.Models
+{
+    public class Agent
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.String)]
+        public string AgentId { get; set; }
+        [BsonElement("authToken")]
+        public string AuthToken { get; set; }
+    }
+}
